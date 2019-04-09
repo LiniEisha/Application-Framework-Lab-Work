3305196a210d2a89f756d927f69755783079bbb8
LiniEisha <linieesk@gmail.com>
2019-04-09 14:13:08 +0530

    add labsheet 3

---
+++ Labsheet_03/ReactJS/main.jsx
@@ -0,0 +1,8 @@
+'use strict';
+
+import React from 'react';
+import {render} from 'react-dom';
+import AppComponent from './AppContainer.jsx';
+/*import AppContainer from "./AppContainer";*/
+
+render(<AppComponent/>, document.getElementById('app'));
